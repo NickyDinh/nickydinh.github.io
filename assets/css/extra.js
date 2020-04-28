75d11de7ead2ca9835672eb372793efd1a7e547c
Nicolas Dinh <Nickydinhh@gmail.com>
2020-04-28 10:52:18 -0500

    Add files via upload

---
+++ assets/css/extra.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 60,
+    backSpeed: 40,
+    startDelay: 1000,
+    loop: false,
+    loopCount: Infinity,
+    onComplete: function(self) { prettyLog('onComplete ' + self) },
+    preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
+    onStringTyped: function(pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
+    onLastStringBackspaced: function(self) { prettyLog('onLastStringBackspaced ' + self) },
+    onTypingPaused: function(pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
+    onTypingResumed: function(pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
+    onReset: function(self) { prettyLog('onReset ' + self) },
+    onStop: function(pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
+    onStart: function(pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
+    onDestroy: function(self) { prettyLog('onDestroy ' + self) }
+  });
